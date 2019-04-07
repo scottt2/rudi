2f552ec36d52abbaf87e9749686a86b4762e5ab9
tyler <scottt2@uw.edu>
2019-04-06 18:51:08 -0700

    Handle incoming typed message

--- lib/rudi_web/channels/user_prompt_channel.ex
+++ lib/rudi_web/channels/user_prompt_channel.ex
@@ -4,4 +4,8 @@ defmodule RudiWeb.UserPromptChannel do
   def join("user_prompt:" <> user_prompt_id, _params, socket) do
     { :ok, assign(socket, :user_prompt_id, user_prompt_id) }
   end
+
+  def handle_in("typed", params, socket) do
+    {:noreply, socket}
+  end
 end
